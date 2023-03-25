b91b7480a35a43cf78480cdbb64ce19e29027cad
Alamot <antalamot@gmail.com>
2023-03-25 15:59:56 -0400

    minor edit

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const app = new express();
 
 /*This tells the server to use the client 
@@ -13,10 +14,10 @@ app.use(cors_app());
 variables that you set up in the .env file*/
 
 // const dotenv = require('dotenv');
-// dotenv.config();
+dotenv.config();
 
-// const api_key = process.env.API_KEY;
-// const api_url = process.env.API_URL;
+const api_key = process.env.API_KEY;
+const api_url = process.env.API_URL;
 
 function getNLUInstance() {
     /*Type the code to create the NLU instance and return it.
@@ -25,7 +26,7 @@ function getNLUInstance() {
     const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
     const { IamAuthenticator } = require('ibm-watson/auth');
     const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
-        version: '2021-08-01',
+        version: '2022-04-07',
         authenticator: new IamAuthenticator ({
             apikey: api_key
         }),
@@ -56,7 +57,7 @@ app.get("/url/emotion", (req,res) => {
          }
      
       const naturalLanguageUnderstanding = getNLUInstance();
-     
+
       naturalLanguageUnderstanding.analyze(analyzeParams)
       .then(analysisResults => {
          //Please refer to the image to see the order of retrieval
@@ -96,21 +97,23 @@ app.get("/url/sentiment", (req,res) => {
 //The endpoint for the webserver ending with /text/emotion
 app.get("/text/emotion", (req,res) => {
     let textToAnalyze = req.query.text
-    const analyzeParams = 
-    {
+    const analyzeParams = {
         "text": textToAnalyze,
-        "features": {
-            "keywords": {
-                "emotion": true,
-                "limit": 1
+        'features': {
+            'keywords': {              
+              'emotion': true,
+              'limit': 1
             }
-        }
+        }    
     }
     
     const naturalLanguageUnderstanding = getNLUInstance();
     
+    console.log(analyzeParams);
     naturalLanguageUnderstanding.analyze(analyzeParams)
     .then(analysisResults => {
+
+      console.log(analysisResults);
         //Retrieve the emotion and return it as a formatted string
         return res.send(analysisResults.result.keywords[0].emotion,null,2);
     })
